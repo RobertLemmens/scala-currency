c32ab53a8e7037dd5f486b943ad5759e6c21bb4d
Robert Lemmens <robert@srdi.nl>
2018-04-12 02:30:18 +0200

    Removed target

--- build.sbt
+++
@@ -1,5 +0,0 @@
-name := "scala-currency"
-
-version := "0.1"
-
-scalaVersion := "2.12.5"
--- src/main/scala/nl/codebulb/core/implicits/package.scala
+++
@@ -1,9 +0,0 @@
-package nl.codebulb.core
-
-/**
-  * @author Robert Lemmens 
-  *         11-4-18
-  */
-package object implicits extends CurrencyImplicits with operations {
-
-}
